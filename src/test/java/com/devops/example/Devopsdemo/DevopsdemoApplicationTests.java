95a269746e841c33727b9f16b14f78f60634ec94
Anil.Kumar <dayyalaanil7904@gmail.com>
2024-06-03 22:32:46 +0530

    Changes

---
+++ src/test/java/com/devops/example/Devopsdemo/DevopsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.example.Devopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
